29004b9cb558907bdad67b02522ab53022520b90
manojap <manojap@outlook.com>
2021-01-20 23:15:37 +0530

    .

--- src/TT/routes/routes.ts
+++
@@ -1,6 +0,0 @@
-import Controller from '../controllers/Controller.ts';
-import { oak } from '../deps.ts';
-const router = new oak.Router();
-router.get('/about', Controller.index)
-router.get('/', Controller.index)
- export default router;
